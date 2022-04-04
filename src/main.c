a0fb2671da70b0362d33c1f51086ce96d5719046
Trojak, Jan <trojakjan24@gmail.com>
2022-04-04 21:13:18 +0200

    Povolit, pokud nejde pustic capabilities

--- src/main.c
+++ src/main.c
@@ -8,8 +8,7 @@ int stringIsZero( const char* str1 )
 int main( int argc , const char* argv[] )
 {
 	cap_value_t whiteList = {CAP_SYS_TIME};
-	if ( WhiteListCapabilities(&whiteList, 1) != 0 )
-	{ return 4; }
+	WhiteListCapabilities(&whiteList, 1);
 	if ( argc != 2 )
 	{ return 1; }
 	long seconds = atol( argv[1] );
